a66dabc2051e1aa7d524344a0629ad9a54e297e0
Jona <jona@solid-kiss.be>
2025-10-03 15:31:20 +0200

    first commit

---
+++ src/app/layout/component/entitiesItems.ts
@@ -0,0 +1,10 @@
+export let entitiesItems = [
+    [
+        // Shipster will add entities
+        // {
+        //     label: 'Articles',
+        //     icon: 'pi pi-fw pi-pencil',
+        //     routerLink: ['/articles']
+        // }
+    ]
+];
